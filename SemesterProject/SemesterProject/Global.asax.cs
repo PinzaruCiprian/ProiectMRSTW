850467516fe848183e3d991bdf8b69fd2f45d9e8
Ciprian <ciprian.pinzaru@isa.utm.md>
2024-02-22 22:07:02 +0200

    Added the packages required, structured the main view and some extra.

--- SemesterProject/SemesterProject/Global.asax.cs
+++ SemesterProject/SemesterProject/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SemesterProject.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -16,6 +18,7 @@ namespace SemesterProject
             // Code that runs on application startup
            AreaRegistration.RegisterAllAreas();
            RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }
+           BundleConfig.RegisterBundles(BundleTable.Bundles);
+          }
     }
 }
