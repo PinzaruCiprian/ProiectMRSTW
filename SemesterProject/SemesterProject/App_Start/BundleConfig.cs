96fdd38ea33ca1468b5ea8929df74acda5674be2
Ciprian <ciprian.pinzaru@isa.utm.md>
2024-02-25 15:50:30 +0200

    Created BundleConfig class, all bundles will be added there.

---
+++ SemesterProject/SemesterProject/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SemesterProject.App_Start
+{
+     public class BundleConfig
+     {
+          public static void RegisterBundles(BundleCollection bundles)
+          {
+          }
+     }
+}
