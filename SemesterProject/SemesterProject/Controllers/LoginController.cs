f02d120444fb5b9340426d298680280f07394a1b
Daniel05 <tomadaniel05@gmail.com>
2024-03-04 15:18:34 +0200

    Added View for Login

--- SemesterProject/SemesterProject/Controllers/LoginController.cs
+++ SemesterProject/SemesterProject/Controllers/LoginController.cs
@@ -14,48 +14,48 @@ namespace SemesterProject.Controllers
 {
     public class LoginController : Controller
     {
-          private readonly ISession _session;
+          //private readonly ISession _session;
 
-          public LoginController()
-          {
-               var bl = new BussinessLogic();
-               _session = bl.GetSessionBL();
-          }
+          //public LoginController()
+          //{
+          //     var bl = new BussinessLogic();
+          //     _session = bl.GetSessionBL();
+          //}
 
           public ActionResult Index()
           {
                return View();
           }
 
-          [HttpPost]
-          [ValidateAntiForgeryToken]
-          public ActionResult Index(UserLogin login)
-          {
-               if (ModelState.IsValid)
-               {
-                    ULoginData data = new ULoginData
-                    {
+          //[HttpPost]
+          //[ValidateAntiForgeryToken]
+          //public ActionResult Index(UserLogin login)
+          //{
+          //     if (ModelState.IsValid)
+          //     {
+          //          ULoginData data = new ULoginData
+          //          {
 
-                         Credential = login.Credential,
-                         Password = login.Password,
-                         LoginIp = Request.UserHostAddress,
-                         LoginDateTime = DateTime.Now
-                    };
-                    var userLogin = _session.UserLogin(data);
-                    if (userLogin.Status)
-                    {
-                         //ADD COOKIE
+          //               Credential = login.Credential,
+          //               Password = login.Password,
+          //               LoginIp = Request.UserHostAddress,
+          //               LoginDateTime = DateTime.Now
+          //          };
+          //          var userLogin = _session.UserLogin(data);
+          //          if (userLogin.Status)
+          //          {
+          //               //ADD COOKIE
 
-                         return RedirectToAction("Index", "Home");
-                    }
-                    else
-                    {
+          //               return RedirectToAction("Index", "Home");
+          //          }
+          //          else
+          //          {
 
-                         ModelState.AddModelError("", userLogin.StatusMsg);
-                         return View();
-                    }
-               }
-               return View();
-          }
+          //               ModelState.AddModelError("", userLogin.StatusMsg);
+          //               return View();
+          //          }
+          //     }
+          //     return View();
+          //}
     }
 }
