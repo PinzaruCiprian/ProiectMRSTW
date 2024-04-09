da1e8dae66bfe7549da461745e652d32b5856815
Daniel05 <tomadaniel05@gmail.com>
2024-04-09 16:21:33 +0300

    Added SearchPage View and Items

---
+++ SemesterProject/SemesterProject/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿using eUseControl.BussinessLogic.AppBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SemesterProject.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult SearchPage()
+        {
+               using (var db = new UserContext())
+               {
+                    ViewBag.tickets = 8;
+               }
+                    return View();
+        }
+    }
+}
